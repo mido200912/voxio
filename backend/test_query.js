84be369a1108420e59bbd3bf51c389b06c2a5181
mido200912 <mohammedalsyedalsyed12@gmail.com>
2026-05-16 06:28:09 +0300

    feat: add test script to verify user database queries

--- backend/test_query.js
+++ backend/test_query.js
@@ -5,6 +5,7 @@ import User from './models/User.js';
 async function run() {
   await mongoose.connect(process.env.MONGO_URI);
   console.log("Connected to MongoDB.");
+
   
 
   const emailToTest = 'random.email12345@test.com';
